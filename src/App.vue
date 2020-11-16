871db5ec25b7458ec256d8793ea8e175af497df6
ISol <599304776@qq.com>
2020-11-16 13:50:08 +0800

    first time

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Feng/>
+</template>
+
+<script>
+import Feng from './components/Feng.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Feng
+  }
+}
+</script>
---
+++ src/components/Feng.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我的第一个组件</div>
+</template>
+
+<script>
+  export default {
+    name: "Feng"
+  }
+</script>
+
+<style  scoped>
+
+</style>
