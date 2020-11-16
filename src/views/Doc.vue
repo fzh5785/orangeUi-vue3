54ef71e6f83ae2a75ee20c7c7d1f7de4a191d3e6
ISol <599304776@qq.com>
2020-11-16 15:57:20 +0800

    整体css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="layout">
+    <Topnav class="nav"/>
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -27,38 +27,85 @@
 </template>
 
 <script lang="ts">
-  import Topnav from "../components/Topnav.vue"
-  import {inject,Ref} from 'vue';
+  import Topnav from '../components/Topnav.vue';
+  import {inject, Ref} from 'vue';
 
   export default {
-    name: "Doc",
+    name: 'Doc',
     components: {Topnav},
     setup() {
       const asideVisible = inject<Ref<boolean>>('asideVisible');
-      return {asideVisible}
+      return {asideVisible};
     }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
-aside{
-  background: lightblue;
-  width: 150px;
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 
+    >.nav {
+      flex-shrink: 0;
+    }
 
-  >h2{
-    margin-bottom: 4px;
-  }
-  >ol{
-    >li{
-      padding: 4px 0;
+    >.content {
+      flex-grow: 1;
+      padding-top: 10px;
+      padding-left: 156px;
+
+      @media (max-width: 500px) {
+        padding-left: 0;
+      }
     }
   }
-  @media (max-width:500px) {
+
+  .content {
+    display: flex;
+
+    >aside {
+      flex-shrink: 0;
+    }
+
+    >main {
+      flex-grow: 1;
+      padding: 16px;
+      background: white;
+    }
+  }
+
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
-    padding: 70px 16px 16px 16px;
+    padding-top: 70px;
+    height: 100%;
+
+    >h2 {
+      margin-bottom: 4px;
+      padding: 0 16px;
+    }
+
+    >ol {
+      >li {
+        >a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+
+        .router-link-active {
+          background: white;
+        }
+      }
+    }
   }
-}
+
+  main {
+    overflow: auto;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <Topnav/>
-    <div class="banner">
-      <h1>橘子UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
-      <p class="actions">
-        <a>GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>橘子UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
+        <p class="actions">
+          <a>GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -22,26 +25,84 @@
 </script>
 
 <style lang="scss" scoped>
+  $green: #02bcb0;
+  $border-radius:4px;
+  $color:#007974;
+
+  .topnavAndBanner {
+    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+    clip-path: ellipse(80% 60% at 50% 40%);
+  }
+
+  .features {
+    margin: 64px auto;
+    width: 400px;
+
+    @media (min-width: 800px) {
+      width: 800px;
+    }
+
+    @media (min-width: 1200px) {
+      width: 1200px;
+    }
+
+    >ul {
+      display: flex;
+      flex-wrap: wrap;
+
+      >li {
+        width: 400px;
+        margin: 16px 0;
+        display: grid;
+        justify-content: start;
+        align-content: space-between;
+        grid-template-areas:
+          "icon title"
+          "icon text";
+        grid-template-columns: 80px auto;
+        grid-template-rows: 1fr auto;
+
+        >svg {
+          grid-area: icon;
+          width: 64px;
+          height: 64px;
+        }
+
+        >h3 {
+          grid-area: title;
+          font-size: 28px;
+        }
+
+        >p {
+          grid-area: text
+        }
+      }
+    }
+  }
+
   .banner {
+    color: $color;
     padding: 100px 0;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    background: lightgreen;
 
-    > .actions {
-      padding: 8px 0;
+    >.actions {
+      padding: 16px 0;
 
       a {
         margin: 0 8px;
-        background: #fff;
+        color: white;
+        background: $green;
         display: inline-block;
-        $h: 28px;
-        height: $h;
-        line-height: $h;
-        border-radius: $h/2;
-        padding: 0 8px;
+        padding: 12px 24px;
+        border-radius: $border-radius;
+
+        &:hover {
+          text-decoration: none;
+          background: lighten($green, 2%);
+        }
       }
     }
   }
