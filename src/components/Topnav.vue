065277d40faf2969e274515f2eb4bad2afed849c
ISol <599304776@qq.com>
2020-11-16 15:33:17 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,46 +1,74 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo" >LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleAside"></span>
   </div>
 </template>
 
 <script lang="ts">
-  import {inject,Ref} from 'vue';
+  import {inject, Ref} from 'vue';
 
   export default {
-    name: "Topnav",
+    name: 'Topnav',
     setup() {
       const asideVisible = inject<Ref<boolean>>('asideVisible');
-      const toggleAside = ()=> {
-        asideVisible.value = !asideVisible.value
-      }
-      return {toggleAside}
+      const toggleAside = () => {
+        asideVisible.value = !asideVisible.value;
+      };
+      return {toggleAside};
     }
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
-  .topnav{
+  .topnav {
     background: pink;
     display: flex;
     padding: 16px;
     position: relative;
     z-index: 10;
-    >.logo{
+    justify-content: center;
+    align-items: center;
+
+    > .logo {
       max-width: 6em;
       margin-right: auto;
     }
-    >.menu{
+
+    > .menu {
       display: flex;
       white-space: nowrap;
       flex-wrap: nowrap;
-      >li{
-        margin:  0 1em;
+
+      > li {
+        margin: 0 1em;
       }
     }
+
+    > .toggleAside {
+      display: none;
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    @media (max-width: 500px) {
+      > .menu {
+        display: none
+      }
+      > .logo {
+        margin: 0 auto
+      }
+      >.toggleAside{display: inline-block}
+    }
   }
+
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -42,10 +42,8 @@
 aside{
   background: lightblue;
   width: 150px;
-  padding: 70px 16px 16px 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
+
+
   >h2{
     margin-bottom: 4px;
   }
@@ -53,6 +51,12 @@ aside{
     >li{
       padding: 4px 0;
     }
+  }
+  @media (max-width:500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 70px 16px 16px 16px;
   }
 }
 </style>
