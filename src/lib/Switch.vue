d226d9a7054015cbba0194eaed62012c8f2e74be
ISol <599304776@qq.com>
2020-11-17 15:25:02 +0800

    Switch 切换

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,31 +1,49 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{change}">
+    <span></span>
+  </button>
 </template>
 
 <script>
+  import {ref} from "vue"
+
   export default {
-    name: "Switch"
+    name: "Switch",
+    setup() {
+      const change = ref(false)
+      const toggle = () => {
+        change.value = !change.value
+      }
+      return {change, toggle}
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  $h:22px;
+  $h: 22px;
   $h2: $h - 4px;
-  button{
+  button {
     height: $h;
     width: $h2*2;
     border: none;
-    background: blue;
+    background: grey;
     border-radius: $h/2;
     position: relative;
+    >  span {
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      height: $h2;
+      width: $h2;
+      background: white;
+      border-radius: $h2/2;
+    }
   }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background: white;
-    border-radius: $h2/2;
+  button.change {
+    background: blue;
+    >span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
+
 </style>
