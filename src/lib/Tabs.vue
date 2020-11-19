3df0b5dc28d10c3dcf1f142758598cc8c4e186ff
ISol <599304776@qq.com>
2020-11-19 16:08:03 +0800

    Tabs 完成滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="orange-tabs">
-    <div class="orange-tabs-nav">
-      <div v-for="(t,index) in titles" :key="index" class="orange-tabs-nav-item">{{t}}</div>
+    <div class="orange-tabs-nav" ref="container">
+      <div v-for="(t,index) in titles" :key="index" class="orange-tabs-nav-item"
+           @click="select(t)" :class="{selected: t===selected}"
+           :ref="el => { if (el) selectedItem[index] = el }">
+        {{t}}
+      </div>
+      <div class="orange-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="orange-tabs-content">
       <component v-for="(d,index) in defaults" :is="d" :key="index"
-       class="orange-tabs-content-item"
+                 class="orange-tabs-content-item"
       />  <!--渲染 Tab 组件 -->
     </div>
   </div>
@@ -14,9 +19,13 @@
 
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {ref,onMounted,onUpdated} from 'vue';
 
   export default {
     name: 'Tabs',
+    props: {
+      selected: String
+    },
     setup(props, context) {
       //判断 Tabs 子标签的类型
       const defaults = context.slots.default();
@@ -29,7 +38,25 @@
       const titles = defaults.map((tag) => {
         return tag.props.title;
       });
-      return {defaults, titles};
+      const select = (title: String) => {
+        context.emit('update:selected',title)
+      };
+      const selectedItem = ref<HTMLDivElement[]>([])
+      const indicator = ref<HTMLDivElement>(null)
+      const container = ref<HTMLDivElement>(null)
+      const x =()=>{
+        const divs =selectedItem.value
+        const result = divs.filter(div=>div.classList.contains('selected'))[0]
+        const {width}  =result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left:left1} = container.value.getBoundingClientRect()
+        const {left:left2} = result.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      }
+      onMounted(x)
+      onUpdated(x)
+      return {defaults, titles, select,selectedItem,indicator,container};
     }
   };
 </script>
