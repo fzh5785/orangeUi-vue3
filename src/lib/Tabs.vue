279166586da819ec0f2eef5410ce41291ae6c5a8
ISol <599304776@qq.com>
2020-11-19 16:39:34 +0800

    完成 Tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,62 +1,59 @@
 <template>
   <div class="orange-tabs">
     <div class="orange-tabs-nav" ref="container">
-      <div v-for="(t,index) in titles" :key="index" class="orange-tabs-nav-item"
-           @click="select(t)" :class="{selected: t===selected}"
-           :ref="el => { if (t===selected) selectedItem = el }">
-        {{t}}
-      </div>
+      <div class="orange-tabs-nav-item" v-for="(t,index) in titles"
+           :ref="el => { if (t===selected) selectedItem = el }"
+           @click="select(t)" :class="{selected: t=== selected}"
+           :key="index">{{t}}</div>
       <div class="orange-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="orange-tabs-content">
-      <component v-for="(d,index) in defaults" :is="d" :key="index"
-                 class="orange-tabs-content-item"
-      />  <!--渲染 Tab 组件 -->
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
-
 </template>
 
 <script lang="ts">
-  import Tab from './Tab.vue';
-  import {ref,onMounted,onUpdated} from 'vue';
+  import Tab from './Tab.vue'
+  import {computed, ref, onUpdated, onMounted} from 'vue'
 
   export default {
-    name: 'Tabs',
     props: {
       selected: String
     },
     setup(props, context) {
-      //判断 Tabs 子标签的类型
-      const defaults = context.slots.default();
-      defaults.forEach((tag) => {
-        if (tag.type !== Tab) {
-          throw new Error('Tabs 子标签必须为 Tab');
-        }
-      });
-      //拿到 title
-      const titles = defaults.map((tag) => {
-        return tag.props.title;
-      });
-      const select = (title: String) => {
-        context.emit('update:selected',title)
-      };
-      const selectedItem = ref<HTMLDivElement>(null)
-      const indicator = ref<HTMLDivElement>(null)
-      const container = ref<HTMLDivElement>(null)
-      const x =()=>{
-        const {width}  =selectedItem.value.getBoundingClientRect()
+      const selectedItem = ref < HTMLDivElement > (null)
+      const indicator = ref < HTMLDivElement > (null)
+      const container = ref < HTMLDivElement > (null)
+      const x = ()=>{
+        const {width} = selectedItem.value.getBoundingClientRect()
         indicator.value.style.width = width + 'px'
-        const {left:left1} = container.value.getBoundingClientRect()
-        const {left:left2} = selectedItem.value.getBoundingClientRect()
+        const {left: left1} = container.value.getBoundingClientRect()
+        const {left: left2} = selectedItem.value.getBoundingClientRect()
         const left = left2 - left1
         indicator.value.style.left = left + 'px'
       }
       onMounted(x)
       onUpdated(x)
-      return {defaults, titles, select,selectedItem,indicator,container};
+
+      const defaults = context.slots.default()
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+          throw new Error('Tabs 子标签必须是 Tab')
+        }
+      })
+      const current = computed(() => {
+        return defaults.find(tag => tag.props.title === props.selected)
+      })
+      const titles = defaults.map((tag) => {
+        return tag.props.title
+      })
+      const select = (title: string) => {
+        context.emit('update:selected', title)
+      }
+      return {current, defaults, titles, select, selectedItem, indicator, container}
     }
-  };
+  }
 </script>
 
 <style lang="scss">
