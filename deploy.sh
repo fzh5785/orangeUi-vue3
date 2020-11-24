12ba009ee4301fc9a0a10dfdc52cd238d33b008e
ISol <599304776@qq.com>
2020-11-24 16:15:51 +0800

    创建lib/index.ts

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -rf dist
+yarn build
+cd dist
+git init
+git add .
+git commit -m 'deploy'
+git branch -M master
+git remote add origin git@github.com:fzh5785/orangeUi-vue3-website.git
+git push -f -u origin master
+cd ..
--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -10,6 +10,3 @@
   }
 </script>
 
-<style lang="scss" >
-
-</style>
