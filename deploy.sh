c272c3b077fefea214a1289f3966e1a2e30a14b5
ISol <599304776@qq.com>
2020-11-25 14:57:14 +0800

    code

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m 'deploy' &&
 git branch -M master &&
-git remote add origin git@github.com:fzh5785/orangeUi-vue3-website.git &&
-#git remote add origin git@gitee.com:fzh5785/orange-ui-vue3-website.git &&
+#git remote add origin git@github.com:fzh5785/orangeUi-vue3-website.git &&
+git remote add origin git@gitee.com:fzh5785/orange-ui-vue3-website.git &&
 git push -f -u origin master &&
 cd ..
